d1ed83d6683e01fc716888e772fade04a60281ce
Mustafa Ali Khan <khan49605@gmail.com>
2024-03-30 14:48:46 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Anaconda's Cradle 🐍")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+Game_is_on = True
+while Game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        scoreboard.add_score()
+        food.refresh()
+        snake.extend_snake()
+
+    # Detect Collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        Game_is_on = False
+
+    # Detect Collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            Game_is_on = False
+
+
+
+
+
+screen.exitonclick()
+
+# TODO: Create a Snake Body
+# TODO: Move the Snake
+# TODO: Control the Snake
+# TODO: Detect collision with the food.
+# TODO: Create a scoreboard
+# TODO: Detect collision with the wall.
+# TODO: Detect collision with the tail.
+
+
